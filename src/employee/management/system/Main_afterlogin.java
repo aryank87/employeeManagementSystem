5d4606055ea6fe097f626b6d8f0c961d32e0ed2f
Aryan <aryankumar91237@gmail.com>
2025-07-06 12:27:51 +0530

    backgroung and heading added

--- src/employee/management/system/Main_afterlogin.java
+++ src/employee/management/system/Main_afterlogin.java
@@ -1,11 +1,27 @@
 package employee.management.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main_afterlogin extends JFrame {
 
     Main_afterlogin(){
-        setSize(500,400);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/home.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1120,630, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel img = new JLabel(i3);
+        img.setBounds(0,0,1120,630);
+        add(img);
+
+        JLabel heading = new JLabel("Employee Management System");
+        heading.setBounds(340,155,400,40);
+        heading.setFont(new Font("Raleway", Font.BOLD,25));
+        add(heading);
+
+        setSize(1120,630);
+        setLocation(250, 100);
+        setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
