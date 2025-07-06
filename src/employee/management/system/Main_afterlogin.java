006c890ba251e34312aec33fbe008e2b6aae48a9
Aryan <aryankumar91237@gmail.com>
2025-07-06 13:06:07 +0530

    button added

--- src/employee/management/system/Main_afterlogin.java
+++ src/employee/management/system/Main_afterlogin.java
@@ -5,7 +5,31 @@ import java.awt.*;
 
 public class Main_afterlogin extends JFrame {
 
+    JButton addEmployee;
+    JButton viewEmployee;
+    JButton removeEmployee;
+
     Main_afterlogin(){
+
+        addEmployee = new JButton("Add Employee");
+        addEmployee.setBounds(335,270,150,40);
+        addEmployee.setBackground(Color.DARK_GRAY);
+        addEmployee.setForeground(Color.white);
+        addEmployee.setFocusable(false);
+
+        viewEmployee = new JButton("View Employee");
+        viewEmployee.setBounds(565,270,150,40);
+        viewEmployee.setBackground(Color.DARK_GRAY);
+        viewEmployee.setForeground(Color.white);
+        viewEmployee.setFocusable(false);
+
+        removeEmployee = new JButton("Remove Employee");
+        removeEmployee.setBounds(440,370,150,40);
+        removeEmployee.setBackground(Color.DARK_GRAY);
+        removeEmployee.setForeground(Color.white);
+        removeEmployee.setFocusable(false);
+
+        // i add button below.
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/home.jpg"));
         Image i2 = i1.getImage().getScaledInstance(1120,630, Image.SCALE_DEFAULT);
@@ -14,10 +38,15 @@ public class Main_afterlogin extends JFrame {
         img.setBounds(0,0,1120,630);
         add(img);
 
+        // button adding to background
+        img.add(addEmployee);
+        img.add(viewEmployee);
+        img.add(removeEmployee);
+
         JLabel heading = new JLabel("Employee Management System");
         heading.setBounds(340,155,400,40);
         heading.setFont(new Font("Raleway", Font.BOLD,25));
-        add(heading);
+        img.add(heading);
 
         setSize(1120,630);
         setLocation(250, 100);
